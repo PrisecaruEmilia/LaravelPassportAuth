54fa77fec256818a30cbbb1b757f9083df73720d
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-31 15:35:20 +0200

    register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
@@ -17,7 +18,7 @@ class AuthController extends Controller
             if (Auth::attempt($request->only('email', 'password'))) {
                 $auth_user = Auth::user();
                 $user = User::find($auth_user->id);
-                $token = $user->createToken('token')->accessToken;
+                $token = $user->createToken('app')->accessToken;
 
                 return response([
                     'message' => 'Successfully login!',
@@ -34,5 +35,28 @@ class AuthController extends Controller
         return response([
             'message' => 'Invalid Email or Password'
         ], 401);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = $user->createToken('app')->accessToken;
+
+            return response([
+                'message' => 'Registration successful!',
+                'token' => $token,
+                'user' => $user
+            ], 200);
+        } catch (Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
