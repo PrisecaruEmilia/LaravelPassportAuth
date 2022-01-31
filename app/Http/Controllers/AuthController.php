5ef1d05a4e65e18117c73bee0842f96b2de66151
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-31 15:01:20 +0200

    login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+
+    public function login(Request $request)
+    {
+        try {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $auth_user = Auth::user();
+                $user = User::find($auth_user->id);
+                $token = $user->createToken('token')->accessToken;
+
+                return response([
+                    'message' => 'Successfully login!',
+                    'token' => $token,
+                    'user' => $user
+                ], 200);
+            }
+        } catch (Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }
+
+        return response([
+            'message' => 'Invalid Email or Password'
+        ], 401);
+    }
 }
